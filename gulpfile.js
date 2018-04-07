915595f2933f23a0a7e1691b929f7bd506434b42
Adam Millington <infinitecookiehd@gmail.com>
2018-04-07 08:50:21 +0100

    added autoprefixer and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@ const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const uglifyCSS = require('gulp-uglifycss');
 const uglifyJS = require('gulp-uglify-es').default;
-const rename = require('gulp-rename');
-const htmlmin = require('gulp-htmlmin');
-const imageMin = require('gulp-imagemin');
+const rename = require('gulp-rename')
+const htmlmin = require('gulp-htmlmin')
+const autoPrefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('serve', () => {
     browserSync.init({
@@ -14,39 +15,34 @@ gulp.task('serve', () => {
     });
 
     gulp.watch("src/scss/main.scss", ['sass']);
-    gulp.watch("src/css/**/*.css", ['minifyCSS']);
     gulp.watch("src/js/**/*.js", ['scripts']);
-    gulp.watch("src/img/**/*", ['img']);
+    gulp.watch("src/*.html", ['minifyHTML']);
     gulp.watch("dist/*.html").on('change', browserSync.reload)
     gulp.watch("dist/js/**/*.js").on('change', browserSync.reload)
-    gulp.watch("dist/css/**/*.js").on('change', browserSync.reload)
+    gulp.watch("dist/css/**/*.css").on('change', browserSync.reload)
 
 });
 
 gulp.task('sass', () => {
     return gulp.src("src/scss/main.scss")
+      .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
       .pipe(rename("style.css"))
-      .pipe(gulp.dest("src/css/"))
-      .pipe(browserSync.stream());
-  });
-
-gulp.task('minifyCSS', () => {
-    gulp.src('./src/css/**/*.css')
       .pipe(uglifyCSS({
         "maxLineLen": 80,
         "uglyComments": true
       }))
-      .pipe(gulp.dest('./dist/css/'));
-});
-
+      .pipe(autoPrefixer())
+      .pipe(sourcemaps.write("./maps"))
+      .pipe(gulp.dest('./dist/css/'))
+      .pipe(browserSync.stream());
+  });
 gulp.task("scripts", () => {
     return gulp.src("src/js/**/*.js")
-        // .pipe(babel({
-        //     presets: ['env']
-        // }))
+        .pipe(sourcemaps.init())
         .pipe(uglifyJS(/* options */))
-        .pipe(gulp.dest("dist/js/"))
+        .pipe(sourcemaps.write("./maps"))
+        .pipe(gulp.dest("./dist/js/"))
 });
 gulp.task('minifyHTML', () =>{
     return gulp.src('src/*html')
@@ -55,10 +51,6 @@ gulp.task('minifyHTML', () =>{
       }))
       .pipe(gulp.dest('dist'));
 });
-gulp.task('img', () => {
-    return gulp.src('src/img/**/*')
-        .pipe(imageMin())
-        .pipe(gulp.dest('dist/img'))
-});
 
 gulp.task('default', ['minifyHTML','sass','serve']);
+
